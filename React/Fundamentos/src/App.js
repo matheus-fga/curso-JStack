ce2d8a31838f069de4e80351cada15ba5df58730
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-16 22:04:52 -0300

    Using useState

--- React/Fundamentos/src/App.js
+++ React/Fundamentos/src/App.js
@@ -1,22 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Header from './Header';
 import Post from './Post';
 
-const posts = [
-  { title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-  { title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
-  { title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
-];
+function App() { 
+  const [posts, setPosts] = useState([
+    { title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+  ]);
 
-function App() {
-
-  const category = 'Posts da Semana'
+  function handleRefresh() {
+    setPosts((prevState) => [
+      ...prevState,
+      { title: `Title#0${prevState.length + 1}`, 
+      subtitle: `Sub#0${prevState.length + 1}`, 
+      likes: 20 }
+    ]);
+  }
 
   return (
     <Fragment>
       <Header>
-        <h2>Posts da Semana</h2>
+        <h2>
+          Posts da Semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
