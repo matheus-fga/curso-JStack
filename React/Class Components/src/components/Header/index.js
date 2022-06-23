4feb0fd83500f92c0146a480a2f84fce54e9369d
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-23 13:52:44 -0300

    Creating first class component

---
+++ React/Class Components/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header(props) {
+  return (
+    <Container>
+      <h1>JStack's Blog</h1>
+      <button type="button" onClick={props.onToggleTheme} >
+        {props.selectedTheme === 'light' ? '🌚' : '☀️'}
+      </button>
+    </Container>
+  );
+}
+
---
+++ React/Class Components/src/components/Layout/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Header from '../Header';
+import PostsList from '../PostsList';
+import Footer from '../Footer';
+
+export default function Layout(props) {
+  return (
+    <>
+      <Header 
+        onToggleTheme={props.onToggleTheme} 
+        selectedTheme={props.selectedTheme}
+      />
+      <PostsList />
+      <Footer 
+        onToggleTheme={props.onToggleTheme}
+        selectedTheme={props.selectedTheme}
+      />
+    </>
+  );
+}
