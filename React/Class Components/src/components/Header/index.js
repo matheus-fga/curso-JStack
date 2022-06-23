16ffcfe7ca357b9a654fdb02c836efc95aff7e6f
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-23 14:41:59 -0300

    Using props in class components

--- React/Class Components/src/components/Header/index.js
+++ React/Class Components/src/components/Header/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Header(props) {
-  return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <button type="button" onClick={props.onToggleTheme} >
-        {props.selectedTheme === 'light' ? '🌚' : '☀️'}
-      </button>
-    </Container>
-  );
+export default class Header extends React.Component {
+  render() {
+    const { onToggleTheme, selectedTheme } = this.props;
+
+    return (
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button type="button" onClick={onToggleTheme} >
+          {selectedTheme === 'light' ? '🌚' : '☀️'}
+        </button>
+      </Container>
+    );
+  };
 }
+
+// export default function Header(props) {
+//   return (
+//     <Container>
+//       <h1>JStack's Blog</h1>
+//       <button type="button" onClick={props.onToggleTheme} >
+//         {props.selectedTheme === 'light' ? '🌚' : '☀️'}
+//       </button>
+//     </Container>
+//   );
+// }
 
