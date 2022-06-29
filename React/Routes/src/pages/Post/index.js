0cbd9032440d8c47adf27ac16b0855c33c946a9d
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-29 14:43:31 -0300

    Using useHistory to navigate programmatically

--- React/Routes/src/pages/Post/index.js
+++ React/Routes/src/pages/Post/index.js
@@ -1,14 +1,21 @@
 import React, { useMemo } from "react";
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams, useLocation, useHistory } from 'react-router-dom';
 
 export default function Post() {
   const params = useParams();
   const { search } = useLocation();
   const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+  const history = useHistory();
+
+  const handleNavigate = () => {
+    history.push('/posts');
+  }
 
   return(
     <>
       <h1>Show Post</h1>
+      <button onClick={handleNavigate}>Voltar para lista de posts</button>
+      <br />
       <span>{params.id}</span>
       <br />
       <span>{queryParams.get('orderBy') || 'Sem query params'}</span>
